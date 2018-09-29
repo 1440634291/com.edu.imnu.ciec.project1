b94d8364bd4984d4cef8e6f53ff719752ce62263
1440634291 <1440634291@qq.com>
2018-09-29 08:57:35 +0800

    Initial Commit

---
+++ com.edu.imnu.ciec.project1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  com.edu.imnu.ciec.project1
+//
+//  Created by s20171103193 on 2018/9/29.
+//  Copyright © 2018年 s20171103193. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
